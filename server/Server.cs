1bd3c6de04100442932bcc6e8bd3b7937e97c4ec
AsimAzizov <azizov.asimkpc322020@mail.ru>
2022-06-01 00:42:11 +0300

    newcommit

---
+++ server/Server.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Text.RegularExpressions;
+using System.IO;
+
+
+namespace server
+{
+    class Server 
+    {
+        TcpListener Listern;
+        public Server(int Port)
+        {
+            Listern = new TcpListener(IPAddress.Any, Port);
+            Listern.Start();
+
+            while (true)
+            {
+                Listern.AcceptTcpClient();
+            }
+        }
+        ~Server()
+        {
+            if (Listern != null)
+            {
+                Listern.Stop();
+            }
+        }
+        
+    }
+}
